25ae846f57859b384a4f0430ccfcf9685b60c432
Nguyen Viet Dat <vietdat1132@gmail.com>
2026-03-17 18:30:45 +0700

    Added configuration

---
+++ lib/config/firebase_config.dart
@@ -0,0 +1,20 @@
+class FirebaseConfig {
+  const FirebaseConfig._();
+
+  static const String webApiKey = 'AIzaSyBVbDgIG3ah9PByiVpS3N3_IaiA1W6ceuI';
+  static const String webAppId = '1:131409528913:web:0f817d2e389f6e5a1dd070';
+  static const String webMessagingSenderId = '131409528913';
+  static const String webProjectId = 'commerceapp-30c05';
+  static const String webAuthDomain = 'commerceapp-30c05.firebaseapp.com';
+  static const String webStorageBucket =
+      'commerceapp-30c05.firebasestorage.app';
+  static const String webMeasurementId = 'G-41E5VQT08V';
+
+  static const String androidApiKey = 'AIzaSyC4AxkQRvwn2GRrfkGPlpO1GnDzzGWKWJw';
+  static const String androidAppId =
+      '1:131409528913:android:646f00ccc94723dd1dd070';
+  static const String androidMessagingSenderId = '131409528913';
+  static const String androidProjectId = 'commerceapp-30c05';
+  static const String androidStorageBucket =
+      'commerceapp-30c05.firebasestorage.app';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'config/app_data_config.dart';
+import 'firebase_options.dart';
 import 'providers/cart_provider.dart';
 import 'providers/order_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (AppDataConfig.useFirebase) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   final cartProvider = CartProvider();
   final orderProvider = OrderProvider();
